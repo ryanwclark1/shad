afc6d447e83adba9c0dbb3bffb23322a21d14c2d
Ryan Clark <ryanc@accentservices.com>
2025-02-07 20:08:56 -0600

    initialize project structure with Vite, React, and TypeScript; add core components and utilities

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+import { ThemeProvider } from "@/components/theme-provider"
+
+
+function App() {
+
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {/* {children} */}
+    </ThemeProvider >
+  )
+}
+
+export default App
